f8b93d839d2e81d910a0668ecc964b3ea44e2c74
ozrobert <oz.robert@o2.pl>
2023-08-01 11:50:21 +0200

    feat: add a background pattern

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,21 @@ export const metadata: Metadata = {
     'A modern interactive agency specializing in web design and development services.',
 }
 
+const BackgroundPattern = () => {
+  return (
+    <div className="absolute inset-0 -z-10 flex justify-center">
+      {/* small screen size */}
+      <div className="mx-4 basis-full border-x border-x-white/10 lg:hidden" />
+      {/* large screen size */}
+      <div className="mx-4 hidden max-w-[1280px] basis-full grid-cols-3 gap-4 lg:grid">
+        <div className="border-x border-x-white/10" />
+        <div className="border-x border-x-white/10" />
+        <div className="border-x border-x-white/10" />
+      </div>
+    </div>
+  )
+}
+
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html lang="en">
@@ -30,6 +45,7 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
           'bg-slate-900 text-slate-100 antialiased'
         )}
       >
+        <BackgroundPattern />
         {children}
       </body>
     </html>
