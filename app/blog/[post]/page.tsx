02522a3ccc08f60ba4d6048dc3bc95e139eb014e
ozrobert <oz.robert@o2.pl>
2023-08-11 05:37:57 +0200

    feat: add blog and post pages

--- app/blog/[post]/page.tsx
+++ app/blog/[post]/page.tsx
@@ -1,5 +1,50 @@
+import { H1, Paragraph } from '@/components/typography/typography'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import Image from 'next/image'
+
 const BlogPost = () => {
-  return <div>BlogPost</div>
+  return (
+    <article className="container">
+      <header className="flex gap-12">
+        <div className="flex flex-1 flex-col justify-between">
+          <H1>Post Title</H1>
+          <Paragraph className="text-gray-500">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Consequatur necessitatibus ipsa porro, dignissimos soluta iste
+            beatae facere? Eos, ratione nobis!
+          </Paragraph>
+          <div className="flex items-center gap-2">
+            <Avatar>
+              <AvatarImage
+                src="/img/post.jpg"
+                alt="author avatar"
+              />
+              <AvatarFallback>JD</AvatarFallback>
+            </Avatar>
+            <span>John Doe</span>
+          </div>
+        </div>
+        <div className="relative h-[300px] flex-1">
+          <Image
+            src="/img/post.jpg"
+            alt="post image"
+            fill
+            className="object-cover"
+          />
+        </div>
+      </header>
+      <div className="mt-12">
+        <Paragraph className="text-justify text-gray-500">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Non natus,
+          nisi dolore dolor quam ratione inventore architecto minima at eveniet,
+          magni perferendis qui odit expedita nobis animi perspiciatis
+          dignissimos repellendus cum? Pariatur cumque asperiores, officia eaque
+          fuga soluta, minima debitis unde suscipit molestias eos? Expedita
+          commodi molestias quibusdam nobis! Eos.
+        </Paragraph>
+      </div>
+    </article>
+  )
 }
 
 export default BlogPost
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,75 @@
+import ContainerWithHorizontalBorders from '@/components/container-with-horizontal-borders/container-with-horizontal-borders'
+import { H1, Paragraph } from '@/components/typography/typography'
+import Image from 'next/image'
+import Link from 'next/link'
+
 const Blog = () => {
-  return <div>Blog</div>
+  return (
+    <ContainerWithHorizontalBorders>
+      <div className="container space-y-6">
+        <Link
+          href="/blog/postId"
+          className="flex items-center gap-4"
+        >
+          <Image
+            src="/img/post.jpg"
+            alt=""
+            width={400}
+            height={250}
+            className="shrink-0 object-cover"
+          />
+          <div>
+            <H1>Post Title</H1>
+            <Paragraph>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia
+              eius possimus fuga doloremque labore tempora nihil temporibus
+              dicta pariatur quae?
+            </Paragraph>
+          </div>
+        </Link>
+        <Link
+          href="/blog/postId"
+          className="flex items-center gap-4"
+        >
+          <Image
+            src="/img/post.jpg"
+            alt=""
+            width={400}
+            height={250}
+            className="shrink-0 object-cover"
+          />
+          <div>
+            <H1>Post Title</H1>
+            <Paragraph>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia
+              eius possimus fuga doloremque labore tempora nihil temporibus
+              dicta pariatur quae?
+            </Paragraph>
+          </div>
+        </Link>
+        <Link
+          href="/blog/postId"
+          className="flex items-center gap-4"
+        >
+          <Image
+            src="/img/post.jpg"
+            alt=""
+            width={400}
+            height={250}
+            className="shrink-0 object-cover"
+          />
+          <div>
+            <H1>Post Title</H1>
+            <Paragraph>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia
+              eius possimus fuga doloremque labore tempora nihil temporibus
+              dicta pariatur quae?
+            </Paragraph>
+          </div>
+        </Link>
+      </div>
+    </ContainerWithHorizontalBorders>
+  )
 }
 
 export default Blog
